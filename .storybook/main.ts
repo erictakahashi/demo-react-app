8a1cd7c8ca0738f8a7968a7ba141ab861c7d1800
Eric Takahashi <erictakahashi@gmail.com>
2024-11-30 13:21:54 -0300

    chore: Config - Add path alias and add base prettier rules

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,16 +1,27 @@
-import type { StorybookConfig } from "@storybook/react-vite";
+import type { StorybookConfig } from '@storybook/react-vite'
+import path from 'path'
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    "@storybook/addon-onboarding",
-    "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
+    '@storybook/addon-onboarding',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
   ],
   framework: {
-    name: "@storybook/react-vite",
+    name: '@storybook/react-vite',
     options: {},
   },
-};
-export default config;
+  viteFinal: async (config) => {
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...config.resolve?.alias,
+        '~': path.resolve(__dirname, '../src'),
+        ' ': path.resolve(__dirname, '..'),
+      }
+    }
+    return config
+  },
+}
+export default config
